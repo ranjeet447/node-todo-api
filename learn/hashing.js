ab8ff6f600bafa34ffe55e9cba5feb7778499ac7
Ranjeet kumar <ranjeetgupta156@gmail.com>
2018-05-16 02:38:25 +0530

    hash password before saving in database

--- learn/hashing.js
+++ learn/hashing.js
@@ -1,5 +1,20 @@
 const jwt = require('jsonwebtoken');
 
+const bcrypt = require('bcryptjs');
+
+var password = '1234asdf';
+
+bcrypt.genSalt(10,(err,salt)=>{
+  bcrypt.hash(password,salt,(err,hash)=>{
+    console.log(hash);
+  });
+});
+
+var hashedPassword  = '$2a$10$PPjg9KNoOKx1NpZvyKoLee1cgZ2xzDZObpqzHcjo5Q6V1ZQPYh2Cu';
+bcrypt.compare(password,hashedPassword,(err,result)=>{
+  console.log(result);
+})
+
 var data = {
   id:10
 }
